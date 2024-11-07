502fb0fa00b126c3b5b493133ba7266816528c53
Hidayatus Sholikhin <hidaysco26@gmail.com>
2024-11-08 00:22:36 +0700

    fix: error module

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,8 +1,10 @@
 import { SidebarLink } from "@/components/SidebarItems";
-import { ClipboardList, HomeIcon, User } from "lucide-react";
+import { ClipboardList, HomeIcon, User, Package } from "lucide-react";
 
 export const defaultLinks: SidebarLink[] = [
   { href: "/dashboard", title: "Dashboard", icon: HomeIcon },
   { href: "/pemesanan", title: "Pemesanan", icon: ClipboardList },
+  { href: "/layanan", title: "Layanan", icon: Package },
   { href: "/pekerja", title: "Manajemen Pekerja", icon: User },
+  { href: "/userManagement", title: "Manajemen User", icon: User },
 ];
